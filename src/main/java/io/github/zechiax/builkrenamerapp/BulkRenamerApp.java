c7727998430798752e60ebda9a7c7259f5ab0b22
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-08-28 14:06:40 +0200

    changes

--- src/main/java/io/github/zechiax/builkrenamerapp/BulkRenamerApp.java
+++ src/main/java/io/github/zechiax/builkrenamerapp/BulkRenamerApp.java
@@ -4,6 +4,7 @@ import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
 import io.github.palexdev.materialfx.css.themes.Themes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,9 +14,13 @@ public class BulkRenamerApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BulkRenamerApp.class.getResource("renamer.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
-        stage.setTitle("Hello!");
+
+        RenamerController renamerController = fxmlLoader.getController();
+        renamerController.setStageAndSetupListeners(stage);
+
+        stage.setTitle("Bulk Renamer");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/io/github/zechiax/builkrenamerapp/RenamerController.java
+++ src/main/java/io/github/zechiax/builkrenamerapp/RenamerController.java
@@ -1,4 +1,65 @@
 package io.github.zechiax.builkrenamerapp;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+import static java.lang.System.Logger.Level.DEBUG;
+import static java.lang.System.Logger.Level.INFO;
+
 public class RenamerController {
+    private final System.Logger logger = System.getLogger(RenamerController.class.getName());
+    @FXML
+    public Button addFilesButton;
+    @FXML
+    public ListView<File> selectedFilesListView;
+    private final ObservableList<File> newSelectedFiles = FXCollections.observableArrayList();
+    private Stage stage;
+
+    @FXML protected void onAddFilesButtonClick() {
+        logger.log(INFO, "Add files button clicked");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select files to rename");
+
+        var files = fileChooser.showOpenMultipleDialog(stage);
+
+        if (files == null) {
+            logger.log(INFO, "No files selected");
+            return;
+        }
+
+        logger.log(INFO, "Files selected: " + files.size());
+        this.newSelectedFiles.addAll(files);
+    }
+
+    private void onNewSelectedFilesChange(ListChangeListener.Change<? extends File> change) {
+        logger.log(INFO, "New files selected: " + newSelectedFiles.size());
+
+        this.selectedFilesListView.setItems(newSelectedFiles);
+
+        this.newSelectedFiles.clear();
+    }
+
+    public void setStageAndSetupListeners(Stage stage) {
+        stage.setOnCloseRequest(event -> {
+            logger.log(INFO, "Stage closed");
+            Platform.exit();
+        });
+
+        this.stage = stage;
+
+        this.addFilesButton.setOnAction(t -> onAddFilesButtonClick());
+
+        this.newSelectedFiles.addListener(this::onNewSelectedFilesChange);
+    }
 }
