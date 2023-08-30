6420cf4b3c6e266d4da07ef66fa10141e0f494ee
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-08-30 20:45:35 +0200

    add move up/down buttons, solve duplicate files issues

--- src/main/java/io/github/zechiax/builkrenamerapp/RenamerController.java
+++ src/main/java/io/github/zechiax/builkrenamerapp/RenamerController.java
@@ -4,6 +4,7 @@ import io.github.zechiax.builkrenamerapp.core.FileToRename;
 import io.github.zechiax.builkrenamerapp.core.Helpers.FileSizeConverter;
 import io.github.zechiax.builkrenamerapp.core.RenameManager;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -12,6 +13,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -19,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static java.lang.System.Logger.Level.*;
 
@@ -46,11 +49,17 @@ public class RenamerController {
     @FXML
     public TableColumn<FileToRename, String> pathColumn = new TableColumn<>();
     @FXML
+    public TableColumn<FileToRename, String> indexColumn = new TableColumn<>();
+    @FXML
     public TextField firstNumberTextField;
     @FXML
     public TextField stepTextField;
     @FXML
     public TextField paddingTextField;
+    @FXML
+    public Button moveUpButton;
+    @FXML
+    public Button moveDownButton;
     @FXML
     private Stage stage;
 
@@ -95,6 +104,14 @@ public class RenamerController {
             var path = file.getAbsolutePath();
             return new SimpleStringProperty(path);
         });
+        indexColumn.setCellValueFactory(cellData -> {
+            var file = cellData.getValue();
+            var index = selectedFiles.indexOf(file) + 1;
+            return new SimpleStringProperty(Integer.toString(index));
+        });
+
+        // Automatically resize the index column
+        //indexColumn.prefWidthProperty().bind(selectedFilesTableView.widthProperty().multiply(0.05));
 
         selectedFilesTableView.refresh();
     }
@@ -103,16 +120,28 @@ public class RenamerController {
         logger.log(INFO, "Add originalFiles button clicked");
 
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select originalFiles to rename");
 
-        var files = fileChooser.showOpenMultipleDialog(stage);
+        fileChooser.setTitle("Select files to rename");
+        var readonlyFiles = fileChooser.showOpenMultipleDialog(stage);
 
-        if (files == null) {
+        if (readonlyFiles == null) {
             logger.log(INFO, "No originalFiles selected");
             return;
         }
 
-        logger.log(INFO, "Files selected: " + files.size());
+        var files = new ArrayList<>(readonlyFiles);
+
+        // We remove the files that are already in the list
+        files.removeIf(file -> {
+            for (var selectedFile : selectedFiles) {
+                if (selectedFile.getAbsolutePath().equals(file.getAbsolutePath())) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        logger.log(INFO, "New files selected: " + files.size());
         this.selectedFiles.addAll(FileToRename.convertToFilesToRename(files));
         logger.log(DEBUG, "Files added to list");
 
@@ -174,6 +203,52 @@ public class RenamerController {
         updateTableView();
     }
 
+    private void onMoveDownButtonClick(ActionEvent e) {
+        // We move the selected item down by one position
+        var selectedItem = this.selectedFilesTableView.getSelectionModel().getSelectedItem();
+
+        if (selectedItem == null) {
+            return;
+        }
+
+        var index = this.selectedFiles.indexOf(selectedItem);
+
+        if (index == this.selectedFiles.size() - 1) {
+            // Already at the bottom
+            return;
+        }
+
+        this.selectedFiles.remove(selectedItem);
+        this.selectedFiles.add(index + 1, selectedItem);
+        this.selectedFilesTableView.getSelectionModel().clearSelection();
+        this.selectedFilesTableView.getSelectionModel().select(selectedItem);
+
+        updateTableView();
+    }
+
+    private void onMoveUpButtonClick(ActionEvent e) {
+        // We move the selected item up by one position
+        var selectedItem = this.selectedFilesTableView.getSelectionModel().getSelectedItem();
+
+        if (selectedItem == null) {
+            return;
+        }
+
+        var index = this.selectedFiles.indexOf(selectedItem);
+
+        if (index == 0) {
+            // Already at the top
+            return;
+        }
+
+        this.selectedFiles.remove(selectedItem);
+        this.selectedFiles.add(index - 1, selectedItem);
+        this.selectedFilesTableView.getSelectionModel().clearSelection();
+        this.selectedFilesTableView.getSelectionModel().select(selectedItem);
+
+        updateTableView();
+    }
+
     public void setStageAndSetupListeners(Stage stage) {
         stage.setOnCloseRequest(event -> {
             logger.log(INFO, "Stage closed");
@@ -202,5 +277,9 @@ public class RenamerController {
         this.firstNumberTextField.textProperty().addListener(this::counterTextFieldChange);
         this.stepTextField.textProperty().addListener(this::counterTextFieldChange);
         this.paddingTextField.textProperty().addListener(this::counterTextFieldChange);
+
+        // Move buttons
+        this.moveUpButton.setOnAction(this::onMoveUpButtonClick);
+        this.moveDownButton.setOnAction(this::onMoveDownButtonClick);
     }
 }
