18ff42497fd02501338b1645c07f6f548af3c184
Milan Lamplot <106590288+Zechiax@users.noreply.github.com>
2023-09-16 14:57:43 +0200

    Fix run.bat

--- run.bat
+++ run.bat
@@ -1,19 +1,20 @@
-mvn clean package
+call mvn clean package
 
 REM Create the dist folder
-rmdir /s /q dist
-mkdir dist
-mkdir dist\plugins
+rmdir dist /s /q
+md dist\
+md dist\plugins\
 
 REM Copy the jar files
-xcopy app\target\app-*-spring-boot.jar dist\app.jar /s /i
-xcopy plugins\CounterMaskPlugin\target\CounterMaskPlugin-*-all.jar dist\plugins\CounterMaskPlugin.jar /s
-xcopy plugins\DateMaskPlugin\target\DateMaskPlugin-*-all.jar dist\plugins\DateMaskPlugin.jar /s
-xcopy plugins\NameMaskPlugin\target\NameMaskPlugin-*-all.jar dist\plugins\NameMaskPlugin.jar /s
-xcopy plugins\ExtensionMaskPlugin\target\ExtensionMaskPlugin-*-all.jar dist\plugins\ExtensionMaskPlugin.jar /s
+xcopy app\target\app-*-spring-boot.jar dist /s /i
+xcopy plugins\CounterMaskPlugin\target\CounterMaskPlugin-*-all.jar dist\plugins /s
+xcopy plugins\DateMaskPlugin\target\DateMaskPlugin-*-all.jar dist\plugins /s
+xcopy plugins\NameMaskPlugin\target\NameMaskPlugin-*-all.jar dist\plugins /s
+xcopy plugins\ExtensionMaskPlugin\target\ExtensionMaskPlugin-*-all.jar dist\plugins /s
 
 cd dist
 
-java -jar app.jar
+rename app-*.jar app.jar
+call java -jar app.jar
 
 cd ..
