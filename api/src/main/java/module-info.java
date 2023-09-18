2530b81677ed4e0b36e1a6adf2dde317e928faae
Milan Lamplot <106590288+Zechiax@users.noreply.github.com>
2023-09-18 12:54:10 +0200

    Create module-info.java

---
+++ api/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module api {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.io;
+    requires java.logging;
+    requires org.jetbrains.annotations;
+    requires org.pf4j;
+    requires org.slf4j;
+
+    exports io.github.zechiax.api;
+}
